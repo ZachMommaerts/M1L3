dd585ee5af53043758814510502b2bbe184b0931
ZachMommaerts <zmommaerts@gmail.com>
2023-07-13 13:42:33 -0500

    Create a card for Lake Eerie and complete module 3

--- M1L3/ContentView.swift
+++ M1L3/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.systemMint)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0){
+                
+                Image("M1L1_photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack{
+                    
+                    Text("Lake Eerie")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                }
+                
+                Text("Visiting this beautiful Great Lake will take your breath away")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
         }
-        .padding()
     }
 }
 
